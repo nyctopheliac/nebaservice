396f68151e6ef1418a87f02dd852fdcdacbcc752
Arcanjozin <161179287+Arcanjozin@users.noreply.github.com>
2025-03-25 15:53:49 +0000

    inicio do pricing

---
+++ pricing.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>PricingPage</title>
+</head>
+<body>  
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">NebaService</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pricing.php">Pricing</a>
+          </li>
+          <li class="nav-btn">
+          <a href="login.php" class="nav btn" id="pathToRegister">Login</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5"><div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Header</h1>    
+    </div>
+<script src="script.js"></script>
+</body>
+</html>
