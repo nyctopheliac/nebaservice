b9b18c00ee8ad73b27160f067eaba4e1d77faec1
.trollzin <161179287+Arcanjozin@users.noreply.github.com>
2025-06-30 14:56:37 +0100

    q

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand nav-logo" href="#">Neba<span>Service</span></a>
+      <a class="navbar-brand nav-logo"  href="index.php">Neba<span>Service</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
